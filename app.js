d17eecd790f2e1f128adc7f61ca2e3169f81e445
muayedjj <muayedjjawad@gmail.com>
2022-04-20 08:57:11 +0300

    v1.2-js-20-4-2022

---
+++ app.js
@@ -0,0 +1,66 @@
+console.log(username)
+console.log(gender)
+console.log(userage)
+console.log(username)
+
+var username = prompt("Please input your name");
+var gender = prompt("Please input your gender; male or female?");
+if (gender != 'female' && gender != 'male'){
+    var gender = prompt("Please input your gender again; male or female?");
+    }    
+var userage = prompt("How old are you?");
+if (userage <= 0) {
+    var userage = prompt ("That's not possible! "+username+", how old are you again?");
+}
+if (userage <= 0) {
+    var userage = prompt ("That's not possible! "+username+", how old are you again?");
+}
+
+else if (userage > 0) {
+    var welcoming_message = prompt("Would you like to skip the welcoming message? (Y/N)");
+}         
+    if (welcoming_message == 'N' || welcoming_message == 'n') {
+
+        if (gender === 'male') {
+        alert ("Welcome Mr. "+username);}
+
+        if (gender === 'female') {
+        alert ("Welcome Ms. "+username);
+}
+    else if (welcoming_message == 'Y'  || welcoming_message == 'y') {
+    alert( "As you wish" );
+}    
+/*
+}
+if (gender === 'male') {
+    alert ("Welcome Mr"+username)
+}
+else if (gender === 'female') {
+    alert ("Welcome Ms"+username)
+
+}
+else {
+
+}
+*/
+/*switch (welcoming_message) {
+    case "Y"
+    :break;
+    case "N"
+    :if (userage <= 0) {
+    var userage = prompt ("That's not possible! "+username+", how old are you again");
+}
+    else if (gender === 'male') {
+        
+        alert ("Welcome Mr. "+username);
+    }
+    else if (gender === 'female') {
+
+        alert ("Welcome Ms. "+username);
+    }
+    else {
+        var gender = prompt("Please input your gender again; male or female?");
+    }
+    break;
+} 
+*/}
